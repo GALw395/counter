dd31abd18825dd515c735870cd2a45bbd84aa952
GALw395 <galw395@mail.ru>
2024-05-18 14:12:09 +0300

    counter-v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
+
+
 function App() {
+
+  let [count, setCount] = useState(0)
+
+  let incFun = () => count < 5 ? setCount(++count) : count
+
+  let resetFun = () => setCount(count=0)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="conteiner">
+        <div className="conteiner_monitor">
+          <p className="conteiner_monitor-text">{count}</p>
+        </div>
+        <div className="conteiner_button">
+          <button className="conteiner_button-inc" onClick={incFun}>inc</button>
+          <button className="conteiner_button-reset" onClick={resetFun}>reset</button>
+        </div>
+      </div>
+
     </div>
   );
 }
+
+
 
 export default App;
